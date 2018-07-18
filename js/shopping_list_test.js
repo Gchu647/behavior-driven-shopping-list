2f46a205d11ccfab4712633ca5cf947cd7cc3bde
Bronson Avila <bronson.avila@gmail.com>
2018-07-17 16:19:57 -1000

    Write test re: method named "render"

--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -2,6 +2,12 @@ const expect = chai.expect;
 const should = chai.should();
 
 describe('ShoppingListItem', function() {
+  let item;
+
+  beforeEach(function() {
+    item = new ShoppingListItem('Apple', 'Sweet');
+  });
+
   it('should be a Class', function() {
     (new ShoppingListItem().constructor === ShoppingListItem.prototype.constructor).should.equal(true);
   });
@@ -18,14 +24,12 @@ describe('ShoppingListItem', function() {
     (new ShoppingListItem()).hasOwnProperty('is_done').should.equal(true);
   });
 
-  it('should accept 2 arguments: name and description', function() {
-    const item = new ShoppingListItem('Apple', 'Sweet');
-
+  it('should accept 2 arguments: name and description', function () {
     item.name.should.equal('Apple');
     item.description.should.equal('Sweet');
   });
 
-  it('should only accept strings as arguments', function() {
+  it('should only accept strings as arguments', function () {
     const itemNull = new ShoppingListItem('Apple', null);
     const itemObject = new ShoppingListItem('Apple', {});
 
@@ -33,19 +37,23 @@ describe('ShoppingListItem', function() {
     itemObject.description.should.not.be.a('string');
   });
 
-  it('should have a method named "check"', function(){
-    const item = new ShoppingListItem('Apple', 'Sweet');
-
+  it('should have a method named "check"', function () {
     item.check.should.be.a("function");
     item.check();
     item.is_done.should.equal(true);
   });
 
-  it('should have a method named "uncheck"', function(){
-    const item = new ShoppingListItem('Apple', 'Sweet');
-
+  it('should have a method named "uncheck"', function () {
     item.uncheck.should.be.a("function");
     item.uncheck();
     item.is_done.should.equal(false);
+  });
+
+  it('should have a method named "render" that constructs HTML string with <li> tags', function () {
+    item.render.should.be.a('function');
+
+    const htmlString = item.render();
+    htmlString.startsWith('<li').should.equal(true);
+    htmlString.endsWith('</li>').should.equal(true);
   });
 });
