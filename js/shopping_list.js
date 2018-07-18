14b0b6a608048f40381a4dd28811f2518ca566ef
Bronson Avila <bronson.avila@gmail.com>
2018-07-18 09:42:23 -1000

    Create removeItem() method and pass tests

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -10,4 +10,15 @@ class ShoppingList {
       throw new Error('Error!');
     }
   }
+
+  removeItem(item) {
+    if (item && item instanceof ShoppingListItem) {
+      var index = this.items.indexOf(item);
+      this.items.splice(index, 1);
+    } else if (!item) {
+      this.items.pop();
+    } else {
+      throw new Error('Error!');
+    }
+  }
 }
