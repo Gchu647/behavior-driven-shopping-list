bd98d9f41f4e8315052b6a9c75fdbb255616d267
Bronson Avila <bronson.avila@gmail.com>
2018-07-18 18:48:59 -1000

    Fix ShoppingListItem tests

--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -10,19 +10,19 @@ describe('ShoppingListItem', function() {
 
   // Testing if ShoppingListItem instance is created using the ShoppingListItem class
   it('should be a Class', function() {
-    (new ShoppingListItem().constructor === ShoppingListItem.prototype.constructor).should.equal(true);
+    (item.constructor === ShoppingListItem.prototype.constructor).should.equal(true);
   });
 
   it('should have a property named "name"', function() {
-    new ShoppingListItem().hasOwnProperty('name').should.equal(true);
+    item.hasOwnProperty('name').should.equal(true);
   });
 
   it('should have a property named "description"', function() {
-    new ShoppingListItem().hasOwnProperty('description').should.equal(true);
+    item.hasOwnProperty('description').should.equal(true);
   });
 
   it('should have a property named "is_done"', function() {
-    new ShoppingListItem().hasOwnProperty('is_done').should.equal(true);
+    item.hasOwnProperty('is_done').should.equal(true);
   });
 
   it('should accept 2 arguments: name and description', function() {
